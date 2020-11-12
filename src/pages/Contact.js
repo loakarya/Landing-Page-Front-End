62f2c9e1e516a9ca6e9bcd731946900fb649217e
syaina <syainanurf@gmail.com>
2020-11-13 01:00:35 +0700

    added contact interface

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,16 +1,21 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
 
-class Contact extends Component {
-    state = {  }
-    render() {
-        return (
-            <div>
-                <Header />
-                <h1>Kontak</h1>
-                <p>Ini adalah halaman Kontak</p>
+export default function Contact () {
+
+    return (
+        <div id="main">
+            <Header />
+            <div id="content" className="account-container text--center">
+                <h1 className="font-color-primary mb-4">Tetaplah terhubung bersama kami</h1>
+                <Link><div className="account account-whatsapp">via WhatsApp</div></Link>
+                <Link><div className="account account-instagram">viaInstagram</div></Link>
+                <Link><div className="account account-linkedin">via LinkedIn</div></Link>
+                <Link><div className="account account-email">via Email</div></Link>
             </div>
-        );
-    }
+            <Footer />
+        </div>
+    )
 }
-export default Contact;
