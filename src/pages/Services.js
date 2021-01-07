b7cdf0660564238d700ea7fb050b23f6659e2101
adipati27ma <adipati.cs.samsung@gmail.com>
2021-01-07 17:49:39 +0700

    done responsive services page header

--- src/pages/Services.js
+++ src/pages/Services.js
@@ -5,6 +5,8 @@ import Header from '../components/Header/Header';
 import PageHeader from '../components/PageHeader/PageHeader';
 import Footer from '../components/Footer/Footer';
 
+import { Typography } from '@material-ui/core';
+
 export default function Services() {
   return (
     <div id="main">
@@ -12,6 +14,22 @@ export default function Services() {
       <Header />
       <div id="content" className="services-content">
         <PageHeader />
+        <section className="services-section">
+          <Typography
+            variant="h2"
+            align="center"
+            className="services-section-title"
+          >
+            Bagaimana Cara Kerjanya?
+          </Typography>
+          <Typography
+            variant="subtitle2"
+            align="center"
+            className="services-section-subtitle"
+          >
+            Berkolaborasi dengan desainer secara mudah dan menyenangkan
+          </Typography>
+        </section>
       </div>
       <Footer />
     </div>
