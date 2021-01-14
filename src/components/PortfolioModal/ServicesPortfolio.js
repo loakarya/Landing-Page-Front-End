8a099205df3ad8417ef50bee6c188a099af59586
adipati27ma <adipati.cs.samsung@gmail.com>
2021-01-14 22:20:37 +0700

    minimize all portfolio image

--- src/components/PortfolioModal/ServicesPortfolio.js
+++ src/components/PortfolioModal/ServicesPortfolio.js
@@ -10,40 +10,40 @@ import pic2_5 from '../../image/portfolio/original/japandi/5-min.jpg';
 import pic2_6 from '../../image/portfolio/original/japandi/6-min.jpg';
 import pic2_7 from '../../image/portfolio/original/japandi/7-min.jpg';
 
-import pic3_1 from '../../image/portfolio/original/kenko/1-min.png';
-import pic3_2 from '../../image/portfolio/original/kenko/2-min.png';
-import pic3_3 from '../../image/portfolio/original/kenko/3-min.png';
-import pic3_4 from '../../image/portfolio/original/kenko/4-min.png';
-import pic3_5 from '../../image/portfolio/original/kenko/5-min.png';
-import pic3_6 from '../../image/portfolio/original/kenko/6-min.png';
-import pic3_7 from '../../image/portfolio/original/kenko/7-min.png';
-import pic3_8 from '../../image/portfolio/original/kenko/8-min.png';
+import pic3_1 from '../../image/portfolio/original/kenko/1-min.jpg';
+import pic3_2 from '../../image/portfolio/original/kenko/2-min.jpg';
+import pic3_3 from '../../image/portfolio/original/kenko/3-min.jpg';
+import pic3_4 from '../../image/portfolio/original/kenko/4-min.jpg';
+import pic3_5 from '../../image/portfolio/original/kenko/5-min.jpg';
+import pic3_6 from '../../image/portfolio/original/kenko/6-min.jpg';
+import pic3_7 from '../../image/portfolio/original/kenko/7-min.jpg';
+import pic3_8 from '../../image/portfolio/original/kenko/8-min.jpg';
 import pic3_9 from '../../image/portfolio/original/kenko/9-min.jpg';
-import pic3_10 from '../../image/portfolio/original/kenko/10-min.png';
-import pic3_11 from '../../image/portfolio/original/kenko/11-min.png';
+import pic3_10 from '../../image/portfolio/original/kenko/10-min.jpg';
+import pic3_11 from '../../image/portfolio/original/kenko/11-min.jpg';
 import pic3_12 from '../../image/portfolio/original/kenko/12-min.jpg';
-import pic3_13 from '../../image/portfolio/original/kenko/13-min.png';
-import pic3_14 from '../../image/portfolio/original/kenko/14-min.png';
-import pic3_15 from '../../image/portfolio/original/kenko/15-min.png';
+import pic3_13 from '../../image/portfolio/original/kenko/13-min.jpg';
+import pic3_14 from '../../image/portfolio/original/kenko/14-min.jpg';
+import pic3_15 from '../../image/portfolio/original/kenko/15-min.jpg';
 import pic3_16 from '../../image/portfolio/original/kenko/16-min.png';
 
-import pic4_1 from '../../image/portfolio/original/lobby/1-min.png';
-import pic4_2 from '../../image/portfolio/original/lobby/2-min.png';
-import pic4_3 from '../../image/portfolio/original/lobby/3-min.png';
-import pic4_4 from '../../image/portfolio/original/lobby/4-min.png';
-import pic4_5 from '../../image/portfolio/original/lobby/5-min.png';
-import pic4_6 from '../../image/portfolio/original/lobby/6-min.png';
+import pic4_1 from '../../image/portfolio/original/lobby/1-min.jpg';
+import pic4_2 from '../../image/portfolio/original/lobby/2-min.jpg';
+import pic4_3 from '../../image/portfolio/original/lobby/3-min.jpg';
+import pic4_4 from '../../image/portfolio/original/lobby/4-min.jpg';
+import pic4_5 from '../../image/portfolio/original/lobby/5-min.jpg';
+import pic4_6 from '../../image/portfolio/original/lobby/6-min.jpg';
 
 import pic5_1 from '../../image/portfolio/original/office/1-min.jpg';
 import pic5_2 from '../../image/portfolio/original/office/2-min.jpg';
 import pic5_3 from '../../image/portfolio/original/office/3-min.jpg';
 import pic5_4 from '../../image/portfolio/original/office/4-min.jpg';
 
-import pic6_1 from '../../image/portfolio/original/yanies-cake/1-min.png';
-import pic6_2 from '../../image/portfolio/original/yanies-cake/2-min.png';
-import pic6_3 from '../../image/portfolio/original/yanies-cake/3-min.png';
-import pic6_4 from '../../image/portfolio/original/yanies-cake/4-min.png';
-import pic6_5 from '../../image/portfolio/original/yanies-cake/5-min.png';
+import pic6_1 from '../../image/portfolio/original/yanies-cake/1-min.jpg';
+import pic6_2 from '../../image/portfolio/original/yanies-cake/2-min.jpg';
+import pic6_3 from '../../image/portfolio/original/yanies-cake/3-min.jpg';
+import pic6_4 from '../../image/portfolio/original/yanies-cake/4-min.jpg';
+import pic6_5 from '../../image/portfolio/original/yanies-cake/5-min.jpg';
 
 export default function ServicesPortfolio() {
   const imgSrc = [
